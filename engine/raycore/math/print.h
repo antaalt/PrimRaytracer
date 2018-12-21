3c3e2761f6ad50e400174b2a1c3c992cf117ab26
Antoine Altorffer <aaltorffer@orange.fr>
2018-12-21 10:55:00 +0100

    wut

--- engine/raycore/math/print.h
+++ engine/raycore/math/print.h
@@ -16,57 +16,57 @@ namespace geometry {
 
 	inline std::ostream& operator <<(std::ostream& os, const uv2& vec)
 	{
-		std::cout << "uv2(" << vec.u << ", " << vec.v << ")";
+		os << "uv2(" << vec.u << ", " << vec.v << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const vec3& vec)
 	{
-		std::cout << "vec3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+		os << "vec3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const point3& vec)
 	{
-		std::cout << "point3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+		os << "point3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const norm3& vec)
 	{
-		std::cout << "norm3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+		os << "norm3(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const colorHDR& vec)
 	{
-		std::cout << "colorHDR(" << vec.r << ", " << vec.g << ", " << vec.b << ", " << vec.a << ")";
+		os << "colorHDR(" << vec.r << ", " << vec.g << ", " << vec.b << ", " << vec.a << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const color32& vec)
 	{
-		std::cout << "color32(" << vec.r << ", " << vec.g << ", " << vec.b << ", " << vec.a << ")";
+		os << "color32(" << vec.r << ", " << vec.g << ", " << vec.b << ", " << vec.a << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const quat& vec)
 	{
-		std::cout << "quat(" << vec.x << ", " << vec.y << ", " << vec.z << ", " << vec.w << ")";
+		os << "quat(" << vec.x << ", " << vec.y << ", " << vec.z << ", " << vec.w << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const mat3::col& vec)
 	{
-		std::cout << "mat3::col(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ")";
+		os << "mat3::col(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const mat3& vec)
 	{
-		std::cout << "mat3(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ")";
+		os << "mat3(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const mat4::col& vec)
 	{
-		std::cout << "mat4::col(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ", " << vec[3] << ")";
+		os << "mat4::col(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ", " << vec[3] << ")";
 		return os;
 	}
 	inline std::ostream& operator <<(std::ostream& os, const mat4& vec)
 	{
-		std::cout << "mat4(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ", " << vec[3] << ")";
+		os << "mat4(" << vec[0] << ", " << vec[1] << ", " << vec[2] << ", " << vec[3] << ")";
 		return os;
 	}
 }
